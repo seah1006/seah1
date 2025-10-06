1fc44747699668672e57d6bf37a32db372b5626b
seah1006 <ops1520@naver.com>
2025-10-06 14:43:33 +0900

    Create fibonacci_Library.h

---
+++ fibonacci_Library/fibonacci_Library.h
@@ -0,0 +1,13 @@
+#ifndef FIBONACCI_LIBRARY_H
+#define FIBONACCI_LIBRARY_H
+
+
+int fibonacci_iterative(int n);
+
+
+int fibonacci_recursive(int n);
+
+
+void fibonacci_series(int n, int *arr);
+
+#endif
